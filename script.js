cb66d237615aa645260f955a6947779924c27c01
jamshidakramovich14 <jamshidturdiyev761@gmail.com>
2024-04-01 14:06:42 +0500

    8-lesson

---
+++ script.js
@@ -0,0 +1,163 @@
+"use strict"
+
+
+
+// 1-masala start
+
+// let k = +prompt("k sonini kiriting: ");  
+// let n = +prompt("n sonini kiriting: ");  
+
+// for (let i = 1; i <= n; i++) {
+//     console.log(k);
+// }
+
+// 1-masala end
+
+
+
+// 2-masala start
+
+// let a = 4;
+// let b = 9; 
+
+// for (let i = a; i <= b; i++) {
+//   console.log(i);
+// }
+
+// 2-masala end
+
+
+
+
+// 3-masala start
+
+// let a = 2;
+// let b = 7; 
+
+// for (let i = b; i >= a; i--) {
+//   console.log(i);
+// }
+
+// 3-masala end
+
+
+
+
+
+// 4-masala start
+
+// let konfet = 1000.0;
+
+// for (let i = 1; i <= 10; i+= 1) {
+//     console.log(i + "kg/narxi: " + (konfet * i) + " som");
+// }
+
+// 4-masala end
+
+
+
+
+// 5-masala start
+
+// let narx = 1000;
+
+// for (let i = 0.1; i <= 1; i += 0.1) {
+//   console.log(i + "kg/narxi: " + (narx * i) + " soʻm");
+// }
+
+// 5-masala end
+
+
+
+
+
+// 6-masala start
+
+// let narx = 1000; 
+
+// for (let i = 1.1; i <= 2; i += 0.1) {
+//   console.log(i + " kg konfetning narxi: " + (narx * i) + " soʻm");
+// }
+
+
+// 6-masala end
+
+
+
+
+// 7-misol start
+
+// let a = +prompt("A ni kiriting:"); 
+// let b = +prompt("B ni kiriting:"); 
+// let summa = 0;
+
+// for (let i = a; i <= b; i++) {
+//   summa += i;
+// }
+
+// console.log(`a dan b gacha bo'lgan butun sonlar yig'indisi: ${summa}`);
+
+// 7-masala end
+
+
+
+
+// 8-misol start
+
+// let a = +prompt("A ni kiriting:"); 
+// let b =  +prompt("B ni kiriting:"); 
+// let summa = 0;
+
+// for (let i = a; i <= b; i++) {
+//   summa *= i;
+// }
+
+// console.log(`a dan b gacha bo'lgan butun sonlar yig'indisi: ${summa}`);
+
+// 8-masala end
+
+
+
+
+
+// 9-misol start
+
+// let a = prompt("Enter a number(a<b): ")*1;
+// let b = prompt("Enter a number(a<b): ")*1;
+// let sum = 0;
+
+// for(let i = a; i < b; i++){
+//    sum += i * i;
+//    console.log(i);
+// }
+// console.log(`a dan b gacha bo'lgan butun sonlar kvadratlari yigindisi: ${sum}`);
+
+// 9-masala end
+
+
+
+
+
+
+
+// 10-misol start
+
+// let n = +prompt("Enter a number: ")*1;
+// let sum = 0;
+// for(let i = 0+1; i <= n; i++){
+//    sum += 1 / i;
+//    console.log(i, sum);
+// }
+// console.log(sum); console.log(`Yig'indi: ${sum}`);
+
+// 10-masala end
+
+
+
+
+
+
+
+
+
+
